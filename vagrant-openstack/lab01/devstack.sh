ffba6cb363b04356c4c742e5e07ea798f2bca682
Tô Thành Công <tcvn1985@gmail.com>
2021-09-01 16:57:25 +0700

    update
    update

--- vagrant-openstack/lab01/devstack.sh
+++ vagrant-openstack/lab01/devstack.sh
@@ -9,6 +9,7 @@ su - stack -c 'git clone https://opendev.org/openstack/devstack/'
 su - stack -c 'cd devstack && git checkout stable/wallaby'
 echo "COPY LOCAL.CONF..."
 sudo -u stack sh -c 'cp /tmp/local.conf /opt/stack/devstack'
+sudo -u stack sh -c 'cp /tmp/cirros-0.5.2-x86_64-disk.img /opt/stack/devstack/files'
 
 echo "BUILDING DEVSTACK... THIS TAKES AWHILE..."
 su - stack -c './devstack/stack.sh'
