4412ed6172615f7c37a1107746eebb6a0958360e
Tô Thành Công <tcvn1985@gmail.com>
2021-09-20 12:55:37 +0700

    update kolla

---
+++ vagrant-openstack/lab03/bootstrap_ubuntu20.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "[TASK 1] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "[TASK 2] Set root password"
+echo -e "hcdadmin\nhcdadmin" | passwd root >/dev/null 2>&1
+
+# Install package
+echo "[TASK 3] Install net-tools"
+apt update -qq -y >/dev/null 2>&1
+apt install -qq -y net-tools >/dev/null 2>&1
+apt install python3-dev libffi-dev gcc libssl-dev  >/dev/null 2>&1
+
+
+echo "[TASK 4] Install Iptables"
+apt-get install iptables -y  >/dev/null 2>&1
+apt-get install arptables -y  >/dev/null 2>&1
+apt-get install ebtables -y  >/dev/null 2>&1
+
+update-alternatives --set iptables /usr/sbin/iptables-legacy || true
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy || true
+update-alternatives --set arptables /usr/sbin/arptables-legacy || true
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy || true
+
+echo "[TASK 5] Remove package"
+sudo apt remove python3-simplejson python3-pyasn1-modules -y   >/dev/null 2>&1
+
+echo "[TASK 6]Create file test"
+touch cong.txt
