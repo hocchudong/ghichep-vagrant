1a47184d8824858449e960c9b12826852019d154
Tô Thành Công <tcvn1985@gmail.com>
2021-09-23 09:05:52 +0700

    Update bootstrap_ubuntu20.sh

--- vagrant-openstack/lab04/bootstrap_ubuntu20.sh
+++ vagrant-openstack/lab04/bootstrap_ubuntu20.sh
@@ -36,12 +36,8 @@ echo -e "hcdadmin\nhcdadmin" | passwd root >/dev/null 2>&1
 # Install package
 echo "[TASK 3] Install package"
 apt update -qq -y >/dev/null 2>&1
-apt dist-upgrade -y -qq -y >/dev/null 2>&1
-apt install -qq -y net-tools >/dev/null 2>&1
-apt install -qq -y python3-dev libffi-dev gcc libssl-dev  >/dev/null 2>&1
-
-apt install -qq -y python3-pip >/dev/null 2>&1
-pip3 install -U pip  >/dev/null 2>&1
+# apt dist-upgrade -y -qq -y >/dev/null 2>&1
+apt install -qq -y net-tools vim byobu >/dev/null 2>&1
 
 echo "[TASK 4] Config timezone"
 timedatectl set-timezone Asia/Ho_Chi_Minh
