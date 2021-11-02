50eb00264bb65042ba4ce7c75a8a1c7699bc5949
Tô Thành Công <tcvn1985@gmail.com>
2021-11-02 10:13:20 +0700

    update

---
+++ vagrant-docker/bootstrap_ubuntu20.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+## !IMPORTANT ##
+#
+## This script is tested only in the generic/ubuntu2004 Vagrant box
+## If you use a different version of Ubuntu or a different Ubuntu Vagrant box test this again
+#
+notify() {
+        chatid=1977142239
+        token=1117214915:AAF4LFh6uChng056_oTyM6cz9TY4dyAn3YU
+
+if [ $? -eq 0 ]
+then
+  curl -s --data-urlencode "text=I-AM-OK" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+else
+  curl -s --data-urlencode "text=NOT-OK" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+fi
+
+}
+
+# Status
+sendtelegram() {
+        chatid=1977142239
+        token=1117214915:AAF4LFh6uChng056_oTyM6cz9TY4dyAn3YU
+        default_message="Test canh bao"
+
+        curl -s --data-urlencode "text=$@" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+}
+
+sendtelegram "Setup co ban tren node `hostname`"
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "[TASK 3] Enable and Load Kernel modules"
+cat >>/etc/modules-load.d/containerd.conf<<EOF
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+
+echo "[TASK 4] Add Kernel settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system >/dev/null 2>&1
+
+
+## Bo comment cac dong duoi de su dung containerd
+## echo "[TASK 5] Install containerd runtime"
+# apt update -qq >/dev/null 2>&1
+# apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+# mkdir /etc/containerd
+# containerd config default > /etc/containerd/config.toml
+# systemctl restart containerd
+# systemctl enable containerd >/dev/null 2>&1
+
+echo "[TASK 5] Install container runtime"
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt install -y curl ca-certificates gnupg2 gnupg-agent software-properties-common libpq-dev apt-transport-https
+apt-get install -y docker.io
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+apt-mark hold docker.io
+
+echo "[TASK 8] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 9] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+
+
+notify
