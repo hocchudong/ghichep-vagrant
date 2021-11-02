6cf1cba8b6ee6f48e547cb4f36fa486e2b546649
Tô Thành Công <tcvn1985@gmail.com>
2021-11-02 10:29:41 +0700

    update lab

--- vagrant-k8s/bootstrap_ubuntu20.sh
+++ vagrant-k8s/bootstrap_ubuntu20.sh
@@ -104,9 +104,9 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster.example.com     kmaster
-172.16.16.101   kworker1.example.com    kworker1
-172.16.16.102   kworker2.example.com    kworker2
+172.16.70.97  kmaster.example.com     kmaster
+172.16.70.98  kworker1.example.com    kworker1
+172.16.70.99  kworker2.example.com    kworker2
 EOF
 
 notify
