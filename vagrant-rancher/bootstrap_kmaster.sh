011502b33505088a6ff3d9950f6e4c918dc7e839
Tô Thành Công <tcvn1985@gmail.com>
2021-11-14 09:43:58 +0700

    tai lieu rancher

---
+++ vagrant-rancher/bootstrap_kmaster.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+notify() {
+        chatid=1977142239
+        token=1117214915:AAF4LFh6uChng056_oTyM6cz9TY4dyAn3YU
+
+if [ $? -eq 0 ]
+then
+  curl -s --data-urlencode "text=I-AM-OK" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+else
+  curl -s --data-urlencode "text=NOT-OK" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+fi
+
+}
+
+# Status
+sendtelegram() {
+        chatid=1977142239
+        token=1117214915:AAF4LFh6uChng056_oTyM6cz9TY4dyAn3YU
+        default_message="Test canh bao"
+
+        curl -s --data-urlencode "text=$@" "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatid" > /dev/null
+}
+
+sendtelegram "Bat dau cai tren node `hostname`"
+
+echo "[TASK 1] Pull required containers"
+kubeadm config images pull >/dev/null 2>&1
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
+kubeadm init --apiserver-advertise-address=172.16.70.97 --pod-network-cidr=192.168.19.0/24 >> /root/kubeinit.log 2>/dev/null
+
+echo "[TASK 3] Deploy Calico network"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+
+echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
+
+notify
