b3f9c1eb4c2ff55d93e8bb4bc7a4fa0e23f98bde
Tô Thành Công <tcvn1985@gmail.com>
2021-12-13 20:49:52 +0700

    update file
    cap nhat file

--- vagrant-ceph/lab01/bootstrap_ubuntu20.sh
+++ vagrant-ceph/lab01/bootstrap_ubuntu20.sh
@@ -71,9 +71,9 @@ echo -e "hcdadmin\nhcdadmin" | passwd root >/dev/null 2>&1
 
 # Install package
 echo "[TASK 4] Install package"
-apt update -qq -y >/dev/null 2>&1
-apt dist-upgrade -qq -y >/dev/null 2>&1
-apt install -qq -y net-tools git curl vim byobu crudini >/dev/null 2>&1
+apt-get update -qq -y >/dev/null 2>&1
+apt-get dist-upgrade -qq -y >/dev/null 2>&1
+apt-get install -qq -y net-tools git curl vim byobu crudini bc >/dev/null 2>&1
 
 echo "[TASK 5] Config timezone"
 apt-get install chrony -qq -y >/dev/null 2>&1
